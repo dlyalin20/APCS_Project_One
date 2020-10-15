09388d811dee57f35bf7503a18ac4c488e31e68a
dlyalin20 <dlyalin20@stuy.edu>
2020-10-15 11:12:47 -0400

    Checkerboard and Gambler
    Finished Checkerboard and answered Gambler questions for part A

--- Checkerboard.java
+++ Checkerboard.java
@@ -1,5 +1,66 @@
+/*Checkerboard
+  Group Members: Justin Li; Daniel Lyalin; Isabella Ho
+
+  Compilation: javac Checkerboard.java
+  Execution: java Checkerboard N
+
+  Problem Statement:
+    Write a program Checkerboard.java that takes an integer          
+    command-line argument N, and uses two nested for loops to print  
+    an N-by-N "checkerboard" pattern like the one below: a total of  
+    NxN asterisks, where each row has 2N characters (alternating     
+    between asterisks and spaces).
+
+  Sample Execution:L 
+    % java Checkerboard 5
+      * * * * * 
+       * * * * *
+      * * * * * 
+       * * * * *
+      * * * * * 
+
+    % java Checkerboard 4
+      * * * * 
+       * * * *
+      * * * * 
+       * * * *
+
+    Conclusions/Hypotheses: 
+      No conclusions/hypotheses can or need be made.
+*/
 public class Checkerboard {
-    public static void main(String[] args) {
-        
-    }
+
+	public static void main(String[] args) {
+    
+		int N = Integer.parseInt(args[0]);
+		String oddIn = "* ";
+		String evenIn = " *";
+    
+		for (int i = 1; i <= N; i++) {
+
+			if (i % 2 == 0) {
+
+				for (int k = 0; k < N; k++) {
+
+					System.out.print(evenIn);
+
+				}
+
+				System.out.println("");
+
+			}
+
+			else {
+
+				for (int k = 0; k < N; k++) {
+
+					System.out.print(oddIn);
+
+				}
+
+				System.out.println("");
+				
+			}
+		}
+	}
 }
--- Gambler.java
+++ Gambler.java
@@ -1,27 +1,50 @@
-/******************************************************************************
- *  Compilation:  javac Gambler.java
- *  Execution:    java Gambler stake goal N
- *  
- *  Simulates a gambler who start with $stake and place fair $1 bets
- *  until she goes broke or reach $goal. Keeps track of the number of
- *  times she wins and the number of bets she makes. Run the experiment N
- *  times, averages the results, and prints them out.
- *
- *  % java Gambler 50 250 1000
- *  178 wins of 1000
- *  Percent of games won = 17.8
- *  Avg # bets           = 10010.79
- *
- *  % java Gambler 50 150 1000
- *  337 wins of 1000
- *  Percent of games won = 33.7
- *  Avg # bets           = 4863.95
- * 
- *  % java Gambler 50 100 1000
- *  503 wins of 1000
- *  Percent of games won = 50.3
- *  Avg # bets           = 2464.59
- ******************************************************************************/
+/*
+ Group Members: Justin Li; Daniel Lyalin; Isabella Ho
+
+ Compilation: javac Gambler.java
+ Execution: java Gambler stake goal N
+
+ Problem Statement:
+  Simulates a gambler who starts with $stake and places fair $1 bets until she goes 
+  broke or reaches $goal. Keeps track of the number of times she wins and the number 
+  of bets she makes. Runs the experiment N times, averages the results, and prints 
+  them out.
+
+  Sample Execution:
+    % java Gambler 50 250 1000
+        178 wins of 1000
+        Percent of games won = 17.8
+        Avg # bets = 10010.79
+
+    % java Gambler 50 150 1000
+        337 wins of 1000
+        Percent of games won = 33.7   
+        Avg # bets = 4863.95
+  
+    % java Gambler 50 100 1000
+        503 wins of 1000
+        Percent of games won = 50.3
+        Avg # bets = 2464.59
+
+  Conclusions/Hypotheses:
+    a. If you go to Monte Carlo to try to turn $500 into $2,500 what is your chance of 
+        success?
+        After having run the Gambler simulation with a stake of $500 and a goal of 
+        $2500 1000 times several times, the chances of winning averaged out around 
+        20%, but ranged individually from 18% to 23%.
+    b. How many bets should you expect to make?
+        After having run the Gambler simulation with a stake of $500 and a goal of 
+        $2500 1000 times several times, the average number of bets stayed around one 
+        million, but ranged on individual runs from 900,000 to 1,100,000.
+    c. If you try to turn $1 into $1000 what is your chance of success?
+        After having run the Gambler simulation with a stake of $1 and a goal of 
+        $1000 1000 times several times, the chances of success averaged around 0.1%, 
+        but ranged on individual runs from 0% to 0.5%.
+    d. How many bets should you expect to make?
+        After having run the Gambler simulation with a stake of $1 and a goal of 
+        $1000 1000 times several times, the average number of bets approached 1300, 
+        but ranged on individual runs from 500 to 1500.
+*/  
 
 public class Gambler { 
 
