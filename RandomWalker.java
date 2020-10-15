bd5fbc668b7504d8616d2812497e6e8b550b2516
dlyalin20 <dlyalin20@stuy.edu>
2020-10-15 09:55:41 -0400

    RandomWalker.java
    Completed the code portion of the program

--- RandomWalker.java
+++ RandomWalker.java
@@ -1,5 +1,31 @@
 public class RandomWalker {
     public static void main(String[] args) {
-        
+        int x = 0;
+        int y = 0;
+
+        int steps = Integer.parseInt(args[0]);
+
+        for (int i = 0; i <= steps; i++) {
+            double probability = Math.random();
+
+            if (probability < 0.25) {
+                x++;
+            }
+            else if (probability < 0.5) {
+                y++;
+            }
+            else if (probability < 0.75) {
+                x--;
+            }
+            else {
+                y--;
+            }
+
+            System.out.println("(" + x + "," + y + ")");
+        }
+
+        int squaredDistance = (x*x) + (y*y);
+
+        System.out.println("Squared Distance: " + squaredDistance);
     }
 }
